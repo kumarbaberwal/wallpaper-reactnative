57de3bc342cbe7f87433a61ec9779c2f7bfd2ee9
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-02-15 00:24:04 +0530

    nobottomtabs, tabs, root, bottomsheet

---
+++ app/(tabs)/account.tsx
@@ -0,0 +1,23 @@
+import { DownloadPicture } from "@/components/BottomSheet";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Button, Text, View } from "react-native";
+
+
+export default function account() {
+    const [pictureOpen, setPictureOpen] = useState(false);
+    return <View style={{ flex: 1 }}>
+        <Text>
+            Account page
+        </Text>
+        <Button
+            title="Open bottom sheet"
+            onPress={() => {
+                setPictureOpen(true);
+            }}
+            color={'orange'}
+        >
+        </Button>
+        {pictureOpen && <DownloadPicture onClose={() => setPictureOpen(false)} />}
+    </View>
+}
---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function explore(){
+    return <View>
+        <Text>
+            Explore page
+        </Text>
+    </View>
+}
