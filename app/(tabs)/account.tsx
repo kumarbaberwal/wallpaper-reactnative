6a429dcf61e64472c9aff7c1ea01fb58c08518a3
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-02-19 19:36:05 +0530

    colorScheme set

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,43 +1,43 @@
-import { DownloadPicture } from "@/components/BottomSheet";
 import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 import { Ionicons } from "@expo/vector-icons";
-import { Link } from "expo-router";
-import { useState } from "react";
-import { Button, Pressable, SafeAreaView, StyleSheet, Text, useColorScheme, View } from "react-native";
-
+import { Appearance, Pressable, SafeAreaView, StatusBar, StyleSheet, Text, useColorScheme, View } from "react-native";
 
 export default function Account() {
-    const theme = useColorScheme() ?? "light";
+    const colorScheme = useColorScheme() || 'light';
+    const styles = createStyles({ colorScheme: colorScheme });
     return <SafeAreaView style={{ flex: 1 }}>
-        <Header />
-        <View style={{ flex: 1 }}>
-            <LoginButtons />
-            <ThemeSelector />
-        </View>
-    </SafeAreaView >
+        <ThemedView style={{ flex: 1 }}>
+            <Header styles={styles} />
+            <LoginButtons colorScheme={colorScheme} />
+            <ThemeSelector styles={styles} />
+        </ThemedView>
+        <StatusBar barStyle={'dark-content'}></StatusBar>
+    </SafeAreaView>
 }
 
-function LoginButtons() {
-    const theme = useColorScheme() ?? 'light';
+function LoginButtons({ colorScheme }: { colorScheme: string | null | undefined }) {
     return <>
         <AuthButton
             icon={<Ionicons
                 size={25}
                 name="logo-google"
-                color={theme === 'dark' ? 'white' : 'black'}
+                color={colorScheme === 'dark' ? 'black' : 'white'}
             ></Ionicons>
             }
             label="Sign in"
+            colorScheme={colorScheme}
         >
 
-        </AuthButton >
+        </AuthButton>
         <AuthButton
             icon={<Ionicons
                 size={30}
                 name="logo-apple"
-                color={theme === 'dark' ? 'white' : 'black'}
+                color={colorScheme === 'dark' ? 'black' : 'white'}
             ></Ionicons>}
             label="Sign in"
+            colorScheme={colorScheme}
         >
 
         </AuthButton>
@@ -45,49 +45,55 @@ function LoginButtons() {
 }
 
 
-function Header() {
-    return <View style={styles.header}>
+function Header({ styles }: { styles: any }) {
+    return <ThemedView style={styles.header}>
         <ThemedText style={styles.headerText}>
             Panels
         </ThemedText>
         <ThemedText style={styles.headerSubText}>
             Sign in to save your data
         </ThemedText>
-    </View>
+    </ThemedView>
 }
 
-function ThemeSelector() {
-    return <View style={styles.header}>
+function ThemeSelector({ styles }: { styles: any }) {
+    return <ThemedView style={styles.header}>
         <ThemedText style={styles.headerText}>
             Settings
         </ThemedText>
         <ThemedText style={styles.headerSubText}>
             Theme
         </ThemedText>
-        <View style={styles.themeSelectorContainer}>
-            <ThemeSelectorButton title="System" selected={true} />
-            <ThemeSelectorButton title="Dark" selected={false} />
-            <ThemeSelectorButton title="Light" selected={false} />
-        </View>
-    </View>
+        <ThemedView style={styles.themeSelectorContainer}>
+            <ThemeSelectorButton title="System" selected={true} colorScheme={'light'} styles={styles} />
+            <ThemeSelectorButton title="Dark" selected={false} colorScheme={'dark'} styles={styles} />
+            <ThemeSelectorButton title="Light" selected={false} colorScheme={'light'} styles={styles} />
+        </ThemedView>
+    </ThemedView>
 }
 
-function ThemeSelectorButton({ title, selected }: { title: string, selected: boolean }) {
-    return <Pressable style={styles.themeSelectorChild}>
+function ThemeSelectorButton({ title, selected, colorScheme, styles }: {
+    title: string, selected: boolean, colorScheme: "light" | "dark", styles: any
+}) {
+    return <Pressable style={styles.themeSelectorChild}
+        onPress={() => {
+            Appearance.setColorScheme(colorScheme);
+        }}
+    >
         <ThemedText style={styles.themeSelectorChildText}>
             {title}
         </ThemedText>
     </Pressable>
 }
 
-function AuthButton({ label, icon }: {
-    label: string,
-    icon: any
+function AuthButton({ label, icon, colorScheme }: {
+    label: string;
+    icon: any;
+    colorScheme: string | null | undefined;
 }) {
-    const theme = useColorScheme() ?? "light";
     return <Pressable
         style={{
-            backgroundColor: theme === 'dark' ? 'black' : 'white',
+            backgroundColor: colorScheme === 'dark' ? 'white' : 'black',
             marginHorizontal: 40,
             marginVertical: 10,
             padding: 12,
@@ -99,52 +105,55 @@ function AuthButton({ label, icon }: {
         }}
     >
         {icon}
-        <Text
+        <ThemedText
             style={{
-                color: theme === 'dark' ? 'white' : 'black',
+                color: colorScheme === 'dark' ? 'Black' : 'white',
                 fontSize: 20,
-                fontWeight: 600,
+                fontWeight: "600",
             }}
         >
             {label}
-        </Text>
+        </ThemedText>
     </Pressable>
 }
 
-const styles = StyleSheet.create({
-    headerText: {
-        fontSize: 25,
-        fontWeight: 600,
-        color: 'black',
-    },
-    headerSubText: {
-        fontSize: 15,
-        fontWeight: 300,
-        color: 'black',
-    },
-    header: {
-        padding: 20,
-        margin: 20,
-    },
-    themeSelectorContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginVertical: 20,
-    },
-    themeSelectorChild: {
-        padding: 13,
-        backgroundColor: 'black',
-        borderRadius: 10,
-        borderColor: 'white',
-        borderWidth: 1,
-        flex: 0.3,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    themeSelectorChildText: {
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 500,
-    }
-})
+
+function createStyles({ colorScheme }: {
+    colorScheme: string | null | undefined
+}) {
+    return StyleSheet.create({
+        headerText: {
+            fontSize: 25,
+            fontWeight: "600",
+        },
+        headerSubText: {
+            fontSize: 15,
+            fontWeight: "300",
+        },
+        header: {
+            padding: 20,
+            margin: 20,
+        },
+        themeSelectorContainer: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginVertical: 20,
+        },
+        themeSelectorChild: {
+            padding: 13,
+            backgroundColor: colorScheme === 'dark' ? 'white' : 'black',
+            borderRadius: 10,
+            borderColor: colorScheme === 'dark' ? 'white' : 'white',
+            borderWidth: 1,
+            flex: 0.3,
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+        themeSelectorChildText: {
+            color: colorScheme === 'dark' ? 'black' : 'white',
+            fontSize: 20,
+            fontWeight: "500",
+        },
+    })
+}
